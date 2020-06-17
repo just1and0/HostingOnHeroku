2436671f2c1a71175680e0390c6f049e39d771aa
Oluwatobi Shokunbi (just1and0) <ayoshokz@gmail.com>
2020-06-17 02:11:13 +0100

    edited landingpage title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   HostingOnHeroku
                 </div>
 
                 <div class="links">
